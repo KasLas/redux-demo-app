5ac7eb083f96cfeca98dbd78540b4666512f1947
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-08-29 20:21:43 +0300

    applay middleware

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,9 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
+import thunk from "redux-thunk";
 
 /* the createStoreMethod takes in two parameters
     1. reducers
     2. an initial state at this point it is an empty object
 */
-export const store = createStore(reducers, {});
+export const store = createStore(reducers, {}, applyMiddleware(thunk));
