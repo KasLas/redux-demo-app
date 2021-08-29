0b82d16dbf8ba78e398154e11b4306becba11b90
Kaspars Lasinskis <kasparsll@gmail.comm>
2021-08-29 20:18:43 +0300

    adding a logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
 
 function App() {
   // use The useSelektor hook to access the redux state this can be done at any child component
-  const state = useSelector((state) => state);
+  const account = useSelector((state) => state.account);
 
-  console.log(state);
+  // create a dispatch to call actioncreators
+  const dispatch = useDispatch();
+  const { depositMoneyAction, withdrawMoneyAction } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
 
-  return <div>hello react</div>;
+  return (
+    <div>
+      <h1>{account}</h1>
+      <div>
+        <button
+          onClick={() => {
+            depositMoneyAction(1000);
+          }}
+        >
+          deposit
+        </button>
+        <br />
+        <button
+          onClick={() => {
+            withdrawMoneyAction(1);
+          }}
+        >
+          withdraw
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
